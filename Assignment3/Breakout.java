7296cfee6a2e7821974239dfc39423807d9af4bc
BruceLeeeee <cloudsweet1989@gmail.com>
2013-06-22 12:06:18 -0400

    Assignmenet 3: finished basic functionalities

--- Assignment3/Breakout.java
+++ Assignment3/Breakout.java
@@ -57,15 +57,98 @@ public class Breakout extends GraphicsProgram {
 /** Number of turns */
 	private static final int NTURNS = 3;
 
+/** Animation cycle delay */ 
+	private static final int DELAY = 10;
+
 /* Method: run() */
 /** Runs the Breakout program. */
 	public void run() {
 		/* You fill this in, along with any subsidiary methods */
 		setup();
+		play();
+	}
+	
+	private void play() {
+		createBall();
+		while (!gameOver()) {
+			moveBall();
+			dealWithCollision();
+			pause(DELAY);
+		}
+	}
+	
+	private boolean gameOver() {
+		if (hitLowerWall()) {
+			turnsLeft--;
+			if (turnsLeft > 0) {
+				createBall();
+				return false;
+			} else {
+				printMsg("You lost!");
+				return true;
+			}
+		}
+		
+		if (bricksLeft == 0) {
+			printMsg("You won!");
+			return true;
+		}
+		
+		return false;
+	}
+	
+	private void printMsg(String msg) {
+		double x, y;
+		GLabel label = new GLabel(msg);
+		x = (WIDTH - label.getWidth()) / 2;
+		y = (HEIGHT - label.getAscent()) / 2;
+		add(label, x, y);
+	}
+	private boolean hitLowerWall() {
+		if (ball.getY() + ball.getHeight() >= HEIGHT)
+			return true;
+		return false;
+	}
+	private void dealWithCollision() {
+		GObject collider = getCollidingObject();
+		if (collider == null) {
+			return;
+		} else if (collider == paddle) {
+			vy = -vy;
+		} else {
+			remove(collider);
+			vy = -vy;
+			bricksLeft--;
+		}
+	}
+	
+	private GObject getCollidingObject() {
+		GObject collider;
+		collider = getElementAt(ball.getX(), ball.getY());
+		if (collider != null)
+			return collider;
+		collider = getElementAt(ball.getX() + ball.getWidth(), ball.getY());
+		if (collider != null)
+			return collider;
+		collider = getElementAt(ball.getX() + ball.getWidth(), ball.getY() + ball.getHeight());
+		if (collider != null)
+			return collider;
+		collider = getElementAt(ball.getX(), ball.getY() + ball.getHeight());
+		if (collider != null)
+			return collider;
+		return null;
+	}
+	
+	private void moveBall() {
+		ball.move(vx, vy);
+		if (ball.getX() + ball.getWidth() > WIDTH || ball.getX() < 0)
+			vx = -vx;
+		if (ball.getY() + ball.getHeight() > HEIGHT || ball.getY() < 0)
+			vy = -vy;
 	}
 	
 	/*
-	 * set up the board or initial state of the program
+	 * set up the board and initial state of the program
 	 */
 	private void setup() {
 		createBricks();
@@ -110,6 +193,41 @@ public class Breakout extends GraphicsProgram {
 	}
 	
 	private void createPaddle() {
-		
+		double x, y;
+		paddle = new GRect(PADDLE_WIDTH, PADDLE_HEIGHT);
+		paddle.setFilled(true);
+		paddle.setColor(Color.BLACK);
+		x = (WIDTH - PADDLE_WIDTH) / 2;
+		y = HEIGHT - PADDLE_Y_OFFSET - PADDLE_HEIGHT;
+		add(paddle, x, y);
+		last = new GPoint(x + PADDLE_WIDTH / 2, y);
+		addMouseListeners();
 	}
+	
+	private void createBall() {
+		double x, y;
+		x = WIDTH / 2 - BALL_RADIUS;
+		y = HEIGHT / 2 - BALL_RADIUS;
+		ball = new GOval(BALL_RADIUS * 2, BALL_RADIUS * 2);
+		ball.setFilled(true);
+		ball.setColor(Color.BLUE);
+		add(ball, x, y);
+		vx = rgen.nextDouble(1.0, 3.0);
+		if (rgen.nextBoolean(0.5))
+			vx = -vx;
+		vy = 3.0;
+	}
+	
+	public void mouseMoved(MouseEvent e) {
+		paddle.move(e.getX() - last.getX(), 0);
+		last.setLocation(e.getX(), last.getY());
+	}
+	// instance variables
+	private GRect paddle;
+	private GPoint last;     		/* last mouse position */
+	private GOval ball;
+	private double vx, vy;    		/* v, y velocity of the ball */
+	private int turnsLeft = NTURNS;
+	private int bricksLeft = NBRICKS_PER_ROW * NBRICK_ROWS;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
 }
